2874604675e021ed2aaf70b49182999e2f1c6429
pradeep-raj-2002 <polisettypradeepraj@gmail.com>
2024-11-08 22:31:02 +0530

    Finished My Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+let icon1 = document.getElementById("icon1");
+let icon2 = document.getElementById("icon2");
+let nav_links = document.getElementById("nav-links2")
+function toggleicon(){
+    icon1.classList.toggle("visible")
+    icon2.classList.toggle("visible")
+    nav_links.classList.toggle("visible")
+}
